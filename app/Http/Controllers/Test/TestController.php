1a6f905a269ba743679eccfffa2216b5aef016c1
Drake Huang <drake.huang@macrowell.com.tw>
2016-10-20 15:03:54 +0800

    增加判斷是否有資料夾

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -8,18 +8,25 @@ class TestController extends Controller
     // 產生Html靜態網頁
     public function index()
     {
+        
         // 網頁內文
-        $str = "<html><body><h2>測試產出</h2></body></html>";
+        $str = "<html><body><h2>測試產出=======</h2></body></html>";
         // 儲存路徑
-        $path = "/home/vagrant/d-test/resources/views/test/";
+        $path = "/home/vagrant/d-test/resources/views/activity/";
+        // 先判斷是否有activity個資料夾
+        if (!is_dir($path)) {
+            // 沒有
+            mkdir($path);
+        }
         // 檔案名稱
-        $fileName = "aa.blade.php";
+        $viewPath = "activity03";
+        $fileName = $viewPath . ".blade.php";
 
         $fp = fopen($path . $fileName, "w");
         fwrite($fp, $str);
         fclose($fp);
 
-        return view("test.aa");
+        return view("activity." . $viewPath);
     }
 
 }
